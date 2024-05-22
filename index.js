678e7f29d7071a5e726ec2c3da8d76a6cef17347
GuiTavs7 <7tavs7@gmail.com>
2024-05-21 21:56:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let boletoPago = false;
+
+console.log("Atribuição da variável: " + boletoPago);
+
+if (boletoPago){
+    alert("O boleto está pago!");
+    console.log("O boleto está pago!");
+}
+else if (boletoPago === false){
+    alert("O boleto não está pago!");
+    console.log("O boleto não está pago!");
+}
